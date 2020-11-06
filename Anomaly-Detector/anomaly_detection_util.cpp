c1e39bf2fb142a087dd952a2a0f1d2368966de51
Noam <noamko25@gmail.com>
2020-11-06 18:22:26 +0200

    updated anomaly_detection_util.cpp from last road stone.

--- Anomaly-Detector/anomaly_detection_util.cpp
+++ Anomaly-Detector/anomaly_detection_util.cpp
@@ -1,44 +1,76 @@
-/*
- * animaly_detection_util.cpp
- *
- * Author: write your ID and name here
- */
-
-#include <math.h>
 #include "anomaly_detection_util.h"
+#include <cmath>
 
-float avg(float* x, int size){
-	return 0;
+float mean(float* x, int size) {
+	if(size <= 0) {
+		throw "ERROR: size cant be less or equal to zero";
+	}
+
+	float mean = 0;
+	for (int i = 0; i < size; i++) {
+		mean += *(x + i);
+	}
+	mean /= size;
+	return mean;
 }
 
-// returns the variance of X and Y
 float var(float* x, int size){
-	return 0;
+	if(size <= 0) {
+		throw "size cannot be less or equal to zero!";
+	}
+	float u = 0;
+	float variance = 0;
+	for(int i = 0; i < size; i++) {
+		u += *(x + i);
+		variance += (*(x + i)) * (*(x + i));
+	}
+	u /= size;
+	variance = variance/size - u*u;
+	return variance;
 }
 
-// returns the covariance of X and Y
-float cov(float* x, float* y, int size){
-	return 0;
+float cov(float* x, float* y, int size) {
+	if(size <= 0) {
+		throw "size cannot be less or equal to zero!";
+	}
+	float mean_x = mean(x,size);
+	float mean_y = mean(y,size);
+	float covariance = 0;
+	for (int i = 0; i < size; i++) {
+		covariance += (*(x + i) - mean_x) * (*(y + i) - mean_y);
+	}	
+	covariance /= size;
+	return covariance;
 }
 
-
-// returns the Pearson correlation coefficient of X and Y
-float pearson(float* x, float* y, int size){
-	return 0;
+float pearson(float* x, float* y, int size) {
+	if(size <= 0) {
+		throw "size cannot be less or equal to zero!";
+	}
+	float sigx = std::sqrt(var(x, size));
+	float sigy = std::sqrt(var(y, size));
+	return cov(x, y, size) / (sigx * sigy);
 }
 
-// performs a linear regression and returns the line equation
-Line linear_reg(Point** points, int size){
+Line linear_reg(Point** points, int size) {
+	float x_values[size];
+	float y_values[size];
+	for (int i = 0; i < size; i++) {
+		x_values[i] = points[i]->x;
+		y_values[i] = points[i]->y;
+	}
+	float a = cov(x_values, y_values, size) / var(x_values, size);
+	float b = mean(y_values, size) - a * mean(x_values, size);
 
-	return Line(0,0);
+	return Line(a, b);
 }
 
-// returns the deviation between point p and the line equation of the points
-float dev(Point p,Point** points, int size){
-	return 0;
+float dev (Point p,Line l) {
+	return std::abs(p.y - l.f(p.x));
 }
 
-// returns the deviation between point p and the line
-float dev(Point p,Line l){
-	return 0;
+float dev(Point p, Point** points, int size) {
+	Line l = linear_reg(points, size);
+
+	return dev(p,l);
 }
