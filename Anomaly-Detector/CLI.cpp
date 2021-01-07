5b8db7873cdd685e58594c89ed32bd92cea3f134
Noamko <noamko25@gmail.com>
2021-01-07 11:11:44 +0200

    added a basic CLI

--- Anomaly-Detector/CLI.cpp
+++ Anomaly-Detector/CLI.cpp
@@ -2,14 +2,22 @@
 
 CLI::CLI(DefaultIO* dio) {
     this->dio = dio;
+    this->commands[0] = new UploadCSVCommand(dio);
+    this->commands[1] = new SettingCommand(dio);
+    this->commands[2] = new DetectCommand(dio);
+    this->commands[3] = new DisplayCommand(dio);
+    this->commands[4] = new UploadResultCommand(dio);
+    this->commands[5] = new ExitCommand(dio);
 }
 
-/**
- * @brief simple CLI menu
- * 
- */
 void CLI::start(){
-    this->menu.displayMenu();
+    this->dio->write("Welcome to the Anomaly Detection Server.\n");
+    this->dio->write("Please choose an option:\n");
+    for(int i = 0; i < COMMAND_COUNT; i++) {
+        this->dio->write(to_string(i+1) + ".");
+        this->dio->write(commands[i]->desciption());
+        if(i + 1 != COMMAND_COUNT) this->dio->write("\n");
+    }
 }
 
 CLI::~CLI() {
--- Anomaly-Detector/commands.h
+++ Anomaly-Detector/commands.h
@@ -10,61 +10,116 @@
 #include <vector>
 #include "HybridAnomalyDetector.h"
 
+#define COMMAND_COUNT 6
+
 using namespace std;
 
-class DefaultIO{
+class DefaultIO {
 public:
-	virtual string read()=0;
-	virtual void write(string text)=0;
-	virtual void write(float f)=0;
-	virtual void read(float* f)=0;
-	virtual ~DefaultIO(){}
-        // you may add additional methods here
+	virtual string read() = 0;
+	virtual void write(string text) = 0;
+	virtual void write(float f) = 0;
+	virtual void read(float* f) = 0;
+	virtual ~DefaultIO() {}
+
+	// you may add additional methods here
 };
 
-class StandardIO : public DefaultIO {
-public:
-    virtual string read() {
-		string in;
-		cin >> in;
-		return in;
-	}
-    virtual void write(string text) {
-		cout << text;
-	}
-    virtual void write(float f) {
-		cout << f;
-	}
-    virtual void read(float* f) {
-		//?
-	}
-    virtual ~StandardIO() {}
-
-};
+// you may add here helper classes
 
 
 // you may edit this class
-class Command{
+class Command {
 	DefaultIO* dio;
 public:
-	Command(DefaultIO* dio):dio(dio){}
-	virtual void execute()=0;
+	Command(DefaultIO* dio) :dio(dio) {}
+	virtual void execute() = 0;
 	virtual string desciption() = 0;
-    virtual ~Command(){}
+	virtual ~Command() {}
+
+};
+
+class UploadCSVCommand : public Command {
+	DefaultIO* dio;
+public:
+	UploadCSVCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() override {
+		return "upload a time series csv file";
+	}
+	virtual ~UploadCSVCommand() {}
+};
+
+class SettingCommand : public Command {
+	DefaultIO* dio;
+	public:
+	SettingCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() {
+		return "algorithm settings";
+	}
+	virtual ~SettingCommand() {}
+};
+
+class DetectCommand : public Command {
+	DefaultIO* dio;
+public:
+	DetectCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() {
+		return "detect anomalies";
+	}
+	virtual ~DetectCommand() {}
+};
+
+class DisplayCommand : public Command {
+	DefaultIO* dio;
+public:
+	DisplayCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() {
+		return "display results";
+	}
+	virtual ~DisplayCommand() {}
+};
+
+class UploadResultCommand : public Command {
+	DefaultIO* dio;
+public:
+	UploadResultCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() {
+		return "upload anomalies and analyze results";
+	}
+	virtual ~UploadResultCommand() {}
+};
+
+class ExitCommand : public Command {
+	DefaultIO* dio;
+public:
+	ExitCommand(DefaultIO* dio) : Command(dio) {}
+	virtual void execute() {
+		//todo
+	}
+	virtual string desciption() {
+		return "exit";
+	}
+	virtual ~ExitCommand() {}
 };
 
 // implement here your command classes
-class UploadCommand: public Command {
-public:
-    virtual void execute() {
-		//todo
-	}
-	virtual string desciption(){
-		return "upload a time series csv file";
-    }
-    virtual ~UploadCommand() {}
-};
 
 
 
 #endif /* COMMANDS_H_ */
+
